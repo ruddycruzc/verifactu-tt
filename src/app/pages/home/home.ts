1076c6805a5e58260fc7354fe8ca2025656fc928
Ruddy <ruddy391@gmail.com>
2026-06-18 22:38:27 +0200

    delete:duplicated imports on home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -7,8 +7,6 @@ import { AboutVerifactuSection } from '../../components/sections/about-verifactu
 import { BenefitsSection } from '../../components/sections/benefits-section/benefits-section';
 import { SolutionSection } from '../../components/sections/solution-section/solution-section';
 import { ContactSection } from '../../components/sections/contact-section/contact-section';
-import { Navbar } from '../../components/layout/navbar/navbar';
-import { Footer } from "../../components/layout/footer/footer";
 
 @Component({
   selector: 'app-home',
